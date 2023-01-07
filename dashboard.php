6657301f49d0487fa4cd188af045706c747fb18c
Hadid814 <107250041+Hadid814@users.noreply.github.com>
2023-01-07 17:14:41 +0700

    (incude)
    buat dashboard di folder include
    "ketua"

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<div class="container mt-5">
+    <div class="row text-center">
+        <div class="col-md-4">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Data Barang</h5>
+                    <p class="card-text">Jumlah barang saat ini</p>
+                    <a href="" class="card-link">Lihat data barang</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    
+        
